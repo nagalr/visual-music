2c861292d35f1b22dbe1045bdf39fef51b16f33e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-15 12:22:06 +0000

    Deploy zero-to-mastery/visual-music to github.com/zero-to-mastery/visual-music.git:gh-pages

---
+++ precache-manifest.2d71bb391a22928b3a9a4a7023fe62d4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83f21a40a0337b1c18d03cfb88326bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca3876e644e90d8a4147",
+    "url": "/static/css/main.7cc83e92.chunk.css"
+  },
+  {
+    "revision": "3fe8c6a66668591baa1d",
+    "url": "/static/js/2.41da25ec.chunk.js"
+  },
+  {
+    "revision": "ca3876e644e90d8a4147",
+    "url": "/static/js/main.ed5b49f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a75b3a33956f5d02d0e210d1086abb2e",
+    "url": "/static/media/LogoSVG.a75b3a33.svg"
+  },
+  {
+    "revision": "b927523591859d234259703a70c18903",
+    "url": "/static/media/bg-image2.b9275235.gif"
+  },
+  {
+    "revision": "f1901f87022756b7bdcb312dd400b0db",
+    "url": "/static/media/blackHairAndBubbles.f1901f87.png"
+  },
+  {
+    "revision": "a41c4f3b6cd878826fa39aa3d4f23c1d",
+    "url": "/static/media/download-icon.a41c4f3b.svg"
+  },
+  {
+    "revision": "a127ea7f307ab5ddd58df5e261155940",
+    "url": "/static/media/facebook.a127ea7f.svg"
+  },
+  {
+    "revision": "9d9fa02e64ad019708e795d099a4c256",
+    "url": "/static/media/github.9d9fa02e.svg"
+  },
+  {
+    "revision": "721a1f17706a33b462d85f0c7d38f7bb",
+    "url": "/static/media/linkedIn.721a1f17.svg"
+  },
+  {
+    "revision": "350faac508d087574974413aece278d1",
+    "url": "/static/media/medium.350faac5.svg"
+  },
+  {
+    "revision": "7f7c42ffbf72ebcc43bc03de9f95f2d1",
+    "url": "/static/media/play-icon.7f7c42ff.svg"
+  },
+  {
+    "revision": "b0e1a4d58194713da9a0b15db9215b77",
+    "url": "/static/media/snapshot-icon-white-dark.b0e1a4d5.svg"
+  },
+  {
+    "revision": "6a00f94d18ed8512290a62cef7278237",
+    "url": "/static/media/song-icon.6a00f94d.svg"
+  },
+  {
+    "revision": "3ed12b70e928c41ff4937a7a096d17aa",
+    "url": "/static/media/volume-icon.3ed12b70.svg"
+  }
+]);
